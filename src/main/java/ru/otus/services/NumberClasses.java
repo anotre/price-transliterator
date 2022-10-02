59d035c77d8cfcb687ff827500d43f8ebcdcdb59
a.padalets <notrevispe@yandex.ru>
2022-10-02 14:08:44 +0500

    Added NumberClasses enum

---
+++ src/main/java/ru/otus/services/NumberClasses.java
@@ -0,0 +1,12 @@
+package ru.otus.services;
+
+public enum NumberClasses {
+    UNITS,
+    THOUSANDS,
+    MILLIONS,
+    BILLIONS,
+    TRILLIONS,
+    QUADRILLION,
+    QUINTILLION,
+    SEXTILLION,
+}
