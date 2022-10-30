d872a251c97f594a80b9dbd1299c2194025562f0
anotre <notrevispe@yandex.ru>
2022-10-30 14:46:48 +0500

    Implemented CurrencyFormatter interface - CurrencyFormatterImpl

---
+++ src/main/java/ru/otus/services/CurrencyFormatterImpl.java
@@ -0,0 +1,35 @@
+package ru.otus.services;
+
+import ru.otus.api.domain.Currency;
+import ru.otus.api.domain.IntegerNumber;
+import ru.otus.api.services.CurrencyFormatter;
+
+public class CurrencyFormatterImpl implements CurrencyFormatter {
+
+    Currency currency;
+    IntegerNumber integerNumber;
+    public CurrencyFormatterImpl(Currency currency, IntegerNumber integerNumber) {
+        this.currency = currency;
+        this.integerNumber = integerNumber;
+    }
+
+    @Override
+    public String format() {
+        final int EXPONENTIAL_FUNCTION_BASE = 10;
+        final int LANGUAGE_SPECIFIC_FROM_INCLUSIVE = 10;
+        final int LANGUAGE_SPECIFIC_TO_EXCLUSIVE = 20;
+        String currencyForm;
+        long integerNumberPart = this.integerNumber.getIntegerNumberBeforeRank(2);
+        long firstRank = integerNumberPart % EXPONENTIAL_FUNCTION_BASE;
+
+        if ((integerNumberPart >= LANGUAGE_SPECIFIC_FROM_INCLUSIVE) && (integerNumberPart < LANGUAGE_SPECIFIC_TO_EXCLUSIVE)) {
+            currencyForm = this.currency.getCurrencyNames().get(2);
+        } else if (firstRank == 1) {
+            currencyForm = this.currency.getCurrencyNames().get(0);
+        } else {
+            currencyForm = this.currency.getCurrencyNames().get(1);
+        }
+
+        return currencyForm;
+    }
+}
