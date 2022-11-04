e06c4d644e03a68ca85ba12048cf050666634954
anotre <notrevispe@yandex.ru>
2022-11-04 13:56:45 +0500

    Fixed input type

--- src/main/java/ru/otus/api/services/IntegerNumberParser.java
+++ src/main/java/ru/otus/api/services/IntegerNumberParser.java
@@ -2,6 +2,8 @@ package ru.otus.api.services;
 
 import ru.otus.api.domain.IntegerNumber;
 
+import java.io.IOException;
+
 public interface IntegerNumberParser {
-    IntegerNumber parseIntegerNumber(long integerNumber);
+    IntegerNumber parseIntegerNumber(String integerNumber) throws IOException;
 }
