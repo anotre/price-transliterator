f2e777d86a14fce91495fbe02eb8c222d905fdee
anotre <notrevispe@yandex.ru>
2022-11-05 16:16:02 +0500

    Added Logger interface and it's stub implementation

---
+++ src/main/java/ru/otus/services/LoggerStub.java
@@ -0,0 +1,11 @@
+package ru.otus.services;
+
+import ru.otus.api.services.Logger;
+
+public class LoggerStub implements Logger {
+
+    @Override
+    public void logExceptionMessage(Exception exceptionMessage) {
+
+    }
+}
