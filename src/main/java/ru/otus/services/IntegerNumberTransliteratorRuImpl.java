e62914aa5a03bb58c4c5914dd1bd17f7b45bd022
anotre <notrevispe@yandex.ru>
2022-11-06 17:47:18 +0500

    Removed getIntegerNumberFromParts and removeLeadingZeros methods

--- src/main/java/ru/otus/services/IntegerNumberTransliteratorRuImpl.java
+++ src/main/java/ru/otus/services/IntegerNumberTransliteratorRuImpl.java
@@ -182,25 +182,6 @@ public class IntegerNumberTransliteratorRuImpl implements IntegerNumberTranslite
         return this.transliterationHundredsMap.get(hundredsRankNumber);
     }
 
-    private long getIntegerNumberFromParts(List<Integer> parts) {
-        int power = 0;
-        long number = 0;
-        int indexLastItem = parts.size() - 1;
-        for (int i = indexLastItem; i >= 0; i--) {
-            number += parts.get(i) * Math.pow(EXPONENTIAL_FUNCTION_BASE, power++);
-        }
-        return number;
-    }
-
-    private List<Integer> removeLeadingZeros(List<Integer> parsedIntegerNumber) {
-        for (int i = 0; i < parsedIntegerNumber.size(); i++) {
-            if (parsedIntegerNumber.get(i) != 0) {
-                return parsedIntegerNumber.subList(i, parsedIntegerNumber.size());
-            }
-        }
-        return new ArrayList<Integer>();
-    }
-
     private String transliterateIntegerNumberClassByTriad(List<Integer> integerNumberTriad, int classIndex) {
         String transliteratedClass;
         final int HUNDREDS_RANK = 100;
